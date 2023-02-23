fc47f7be75949ed0b841758c87bc8beb04eac191
adambushman <adambushman@mail.weber.edu>
2023-02-22 21:31:22 -0700

    New colums, multi column selection, to JSON

--- Danfo.js/script.js
+++ Danfo.js/script.js
@@ -1,27 +1,63 @@
-// Basic reading in of data
-
-let myData;
+// Reading in data
 
 d3.csv("../Data/mtcars.csv", 
+
+    // Casting fields to appropriate types
+
     (raw) => {
         return {
             model: raw.model, 
             cyl: raw.cyl, 
+            carb: raw.carb, 
             hp: parseInt(raw.hp),
             mpg: parseInt(raw.mpg)
         }
     }, 
 
     (data) => {
+
+        console.log(data);
+
+        // Creating a Danfo dataframe
+
         df = new dfd.DataFrame(data);
         console.log(df);
 
-        let df_new = df.groupby(["cyl"])
+        // Basic group by
+
+        df.groupby(["cyl"])
             .col(["mpg"])
-            .mean();
+            .mean()
+            .print();
         
-        df_new.print();
+        // Group by with two variables
+
+        df.groupby(["cyl", "carb"])
+            .col(["hp"])
+            .max()
+            .sortValues("carb", {
+                ascending: false
+            })
+            .print();
+
+        // Adding a new column
+
+        df["hp"].print();
+
+        df.addColumn(
+            "log_hp", 
+            df["hp"].values.map(x => Math.log10(x))
+        )
+            .loc({columns: ["model", "log_hp"]})
+            .print();
+
+        // Writing a data frame to JSON object
+
+        let my_test = dfd.toJSON(
+            df.loc({columns: ["model", "hp"]})
+        );
         
-        console.log(df_new);
+        console.log(my_test);
+
     }
 )
