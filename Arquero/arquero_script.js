654729731a98ebaf16892e6393f01703dee0a52b
adambushman <adam.bushman1@gmail.com>
2023-04-15 17:28:14 -0600

    Verb testing

--- Arquero/arquero_script.js
+++ Arquero/arquero_script.js
@@ -16,23 +16,28 @@ d3.csv("../Data/mtcars.csv",
 
     (data) => {
 
-        console.log(data);
+        // From array of objects to Arquero table
+        
+        let aq_tabl = aq.from(data);
 
-        // Testing the "Tidy" function
+        console.log(aq_tabl);
 
-        console.log(Tidy.tidy(data));
+        // From Arquero table to array of objects
+
+        let obj_data = aq_tabl.objects();
+
+        console.log(obj_data);
 
         // Basic group by
 
-        console.log(
-            Tidy.tidy(
-                data, 
-                Tidy.groupBy(
-                    ["cyl", "carb"], 
-                    [Tidy.summarize({avg: mean("mpg")})]
-                )
-            )
-        );
+        let aq_gb = aq_tabl
+            .groupby('cyl')
+            .rollup({
+                mean_mpg: d => op.mean(d.mpg)
+            })
+            .orderby('cyl');
+
+        console.log(aq_gb);
         
         // Group by with two variables
 
