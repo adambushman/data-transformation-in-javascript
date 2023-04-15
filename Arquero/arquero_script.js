637be23e72d2f6706cc5d993bbbe306c304b637a
adambushman <adam.bushman1@gmail.com>
2023-04-15 17:49:58 -0600

    New columns and filtering transforms

--- Arquero/arquero_script.js
+++ Arquero/arquero_script.js
@@ -41,11 +41,30 @@ d3.csv("../Data/mtcars.csv",
         
         // Group by with two variables
 
+        let aq_ga = aq_tabl
+            .groupby(['cyl', 'carb'])
+            .rollup({
+                min_hp: d => op.min(d.hp), 
+                max_hp: d=> op.max(d.hp)
+            });
         
+        console.log(aq_ga);
 
         // Adding a new column
 
+        aq_expanded = aq_tabl
+            .derive({ hp_log: d => op.log10(d.hp)})
+            .select(['model', 'hp', 'hp_log']);
 
+        console.log(aq_expanded);
         
+        // Filtering
+
+        aq_filt = aq_tabl
+            .filter(d => d.hp > 200)
+            .reify()
+            .select('model');
+        
+        console.log(aq_filt);
     }
 )
